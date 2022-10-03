85ce14301954bc42b1c67f51f60e1df2620ec7e2
Mateus Louback <mateuslouback@mbp-de-mateus.lan>
2022-10-03 16:11:44 -0300

    refactor: remove js files and add ts files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import React from 'react';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -52,5 +52,5 @@ export default function Home() {
         </div>
       </main>
     </div>
-  )
+  );
 }
