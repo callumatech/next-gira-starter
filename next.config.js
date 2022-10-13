8543bdb1f4a60540c508c5090c3c72311cf5f360
gibify <gibifyofficial@gmail.com>
2022-10-13 17:50:42 -0300

    chore: devops config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
+
+const apiUrl = process.env.API_URL;
+const newApiUrl = process.env.NEW_API_URL;
+const nextAuthUrl = process.env.NEXTAUTH_URL;
+const nextAuthSecret = process.env.NEXTAUTH_SECRET;
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    API_URL: apiUrl,
+    NEW_API_URL: newApiUrl,
+    NEXTAUTH_URL: nextAuthUrl,
+    NEXTAUTH_SECRET: nextAuthSecret,
+  },
 }
 
 module.exports = nextConfig
